8950cdc5bf5701424f644ef2d178fbd3eac809a3
Juan Ignacio Tornero <84686534+jitornero@users.noreply.github.com>
2025-07-28 00:26:27 -0300

    TC 01 a mitad

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+
+  const quote1= "The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking."
+  await page.goto('https://quotes.toscrape.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  //await expect(page).toHaveTitle(/Playwright/);
+  //await page.pause()
+
+  const quote1_selector = await page.getByText(quote1)
+  console.log("Quote1 encontrado!")
+  await expect(quote1_selector).toBeVisible()
+  const parent = await quote1_selector.locator("//ancestor::div[1]").locator('.author');
+  console.log(parent)
+  expect(parent).toHaveText("Albert Einstein")
+  //expect(await parent.getByText("Albert Einstein", {exact:true})).toBeVisible()
+  
+
 });
 
 test('get started link', async ({ page }) => {
