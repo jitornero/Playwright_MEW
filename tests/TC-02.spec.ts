835eaaeda9f6f0754e6da6c9631a27ef0884f410
Juan Ignacio Tornero <84686534+jitornero@users.noreply.github.com>
2025-07-31 01:44:29 -0300

    TC 02 completo con screenshots pre y post test

--- tests/TC-02.spec.ts
+++ tests/TC-02.spec.ts
@@ -1,24 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test.use({headless:true})
+test.use({ headless: true })
 const TC_index = "-02"
 test(`TC${TC_index}`, async ({ browser, page }) => {
   await page.goto('https://the-internet.herokuapp.com/checkboxes');
 
-  //await page.pause()
+  await page.getByText("checkbox 1").screenshot({ path: `screenshots/TC${TC_index}_box_preTest.png` })
 
-  // const quote1_selector = await page.locator('div.quote').filter({hasText:"he world as we have created"})
-  // console.log("Quote1 encontrado!")
-  // await expect(quote1_selector).toBeVisible()
-  // await quote1_selector.screenshot({path:'screenshots/tc-01.png'});
-  // await expect(quote1_selector.locator(".author")).toHaveText("Albert Einstein")
-  // await quote1_selector.locator("a").count()
-  // await expect(quote1_selector.locator("a").getByText("change")).toBeVisible()
-  await page.locator('input[type="checkbox"]').nth(0)
-  .check()
-    await page.locator('input[type="checkbox"]').nth(0)
-  .screenshot({path:`screenshots/TC${TC_index}.png`})
+  await page.locator('input[type="checkbox"]').nth(0).screenshot({ path: `screenshots/TC${TC_index}_preChecked.png` })
+
+  await page.locator('input[type="checkbox"]').nth(0).check()
+  await expect(page.locator('input[type="checkbox"]').nth(0)).toBeChecked()
+  await page.locator('input[type="checkbox"]').nth(0).screenshot({ path: `screenshots/TC${TC_index}_postCheck.png` })
+
+
+  if (await page.locator('input[type="checkbox"]').nth(1).isChecked()) {
+    await page.locator('input[type="checkbox"]').nth(1).uncheck()
+  }
+
+  expect(await page.locator('input[type="checkbox"]').nth(1)).toBeChecked({ checked: false })
   // .nth(0).check()
-  
 
+  await page.getByText("checkbox 1").screenshot({ path: `screenshots/TC${TC_index}_box_postTest.png` })
 });
